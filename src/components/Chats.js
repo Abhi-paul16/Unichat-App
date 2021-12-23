01a309fa973356adaaeed1e32c369ab583dedad7
Abhishek Pal <abhishek1100861@gmail.com>
2021-12-23 18:44:33 +0530

    Builded login page

---
+++ src/components/Chats.js
@@ -0,0 +1,16 @@
+import { auth } from '../firebase';
+import React from 'react'
+import { ChatEngine } from 'react-chat-engine';
+import { Navigate } from 'react-router-dom';
+
+
+const Chats = () => {
+    return (
+        <div>
+            <h1>Chats</h1>
+        </div>
+    )
+}
+
+
+export default Chats;
